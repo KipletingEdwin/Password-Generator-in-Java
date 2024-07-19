727eb84d07c42f0c44ecb0586f2ce8b1584be9d2
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-07-19 11:14:10 +0300

    finalize on the project

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the length of the password: ");
+        int digit = input.nextInt();
 
+        String lower_cases = "qwertyuiopasdfghjklzxcvbnm";
+        String upper_cases = "QWERTYUIOPASDFGHJKLZXCVBNM";
+        String digits = "0123456789";
+
+        String password = "";
+
+        for(int i = 0; i < digit; i++) {
+            int rand = (int)(3 * Math.random());
+
+            switch(rand){
+                case 0:
+                    rand = (int)(digits.length() * Math.random());
+                    password += digits.charAt(rand);
+                    break;
+                case 1:
+                    rand = (int)(lower_cases.length() * Math.random());
+                    password += lower_cases.charAt(rand);
+                    break;
+                case 2:
+                    rand = (int)(upper_cases.length() * Math.random());
+                    password += upper_cases.charAt(rand);
+                    break;
+            }
+        }
+        System.out.println("Generated password: " + password);
     }
 }
